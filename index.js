795d80fd46c23b7be8e2f4b44f65cac9c990f637
Tumble <tumblegamer@gmail.com>
2019-06-07 16:15:56 +0100

    + to ${}

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ app.get('/success', (req, res) => {
 
 app.get('/', (req, res) => {
   res.send(`Hello<br>
-    <a href="` + app.mountpath + `/auth" target="${loginAuthTarget}">
+    <a href="${app.mountpath}/auth" target="${loginAuthTarget}">
       Log in with ${oauthProvider.toUpperCase()}
     </a>`)
 })
